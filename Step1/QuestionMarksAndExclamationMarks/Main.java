78ec5875baa9771aaf8024a4e7602c61e9a6e08f
hong <whdghk1907@naver.com>
2023-04-18 23:36:31 +0900

    feat($QuestionMarsAndExclamationMarks.java): 백준 10926번 풀이
    시간 복잡도: O(1) (상수 시간)
    공간 복잡도: O(1) (상수 공간)

---
+++ Step1/QuestionMarksAndExclamationMarks/Main.java
@@ -0,0 +1,14 @@
+package Step1.QuestionMarksAndExclamationMarks;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = "??!";
+
+        String input = sc.next();
+
+        System.out.println(input + id);
+    }
+}
