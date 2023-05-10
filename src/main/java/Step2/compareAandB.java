58f598e04eec5d53b7ce2fc495642a9a4de0d49f
hong <whdghk1907@naver.com>
2023-05-10 16:52:35 +0900

    feat($compareAandB): 백준 1330 문제 풀이
    구분 : 조건문을 활용한 단순 출력
    
    메모리 : 14244KB
    시간 : 128ms
    
    시간복잡도 O(1)
    공간복잡도 O(1)

---
+++ src/main/java/Step2/compareAandB.java
@@ -0,0 +1,26 @@
+package Step2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class compareAandB {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = bufferedReader.readLine().split(" ");
+
+        int a = Integer.parseInt(strs[0]);
+        int b = Integer.parseInt(strs[1]);
+
+        if(a > b) {
+            System.out.println(">");
+        }
+
+        if(a < b) {
+            System.out.println("<");
+        }
+
+        if(a == b) {
+            System.out.println("==");
+        }
+    }
+}
